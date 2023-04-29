4fc6e79835cb3a84d0e8d9dce042a54b7a73b4ba
Joseph Bartholomew <contact@joeybartholomew.com>
2023-04-29 11:56:02 -0400

    Week 2 project

---
+++ src/pages/CampsitesDirectoryPage.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import CampsiteDetail from '../features/campsites/CampsiteDetail';
+import CampsitesList from '../features/campsites/CampsitesList';
+import { selectCampsiteById } from '../features/campsites/campsitesSlice';
+// import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
+
+const CampsitesDirectoryPage = () => {
+    const [selectedCampsite, toggleCampsite] = useState(selectRandomCampsite());
+    const [campsiteId, setCampsiteId] = useState(0);
+    const selectedCampsite = selectCampsiteById(campsiteId);
+    return (
+        <Container>
+            {/* <Button onClick={() => toggleCampsite(selectRandomCampsite())}>
+                Select Random Campsite
+            </Button> */}
+            <Row>
+                <Col sm='5' md='7'>
+                    <CampsitesList setCampsiteId={setCampsiteId} />
+                </Col>
+                <Col sm='7' md='5'>
+                    <CampsiteDetail campsite={selectedCampsite} />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default CampsitesDirectoryPage;
